5024da4bfc279da782d384e01eaf8d8b591b19fe
xumaozeng <15611895341@163.com>
2021-06-07 16:45:46 +0800

    中间件

--- src/store/redux-thunk.js
+++ src/store/redux-thunk.js
@@ -1,7 +1,8 @@
 // 手写一个thunk，处理异步的thunk
 export default function thunk({ getState, dispatch }) {
   return next => action => {
-    // next相当于store.dispatch
+    // next是聚合函数applyMiddlerware里的compose()
+    // action相当于dispatch
     if (typeof action === "function") {
       return action(dispatch, getState);
     }
